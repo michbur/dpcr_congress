08e7f108a09847e127786313e7f39ccdca597e64
michbur <michalburdukiewicz@gmail.com>
2014-10-16 21:22:30 +0200

    More space

--- conf_ints.R
+++ conf_ints.R
@@ -6,7 +6,7 @@ c(1L:5*3, 4L:10*5, 6L:10*10)
 get_ints <- function(number_of_exps) {
   adpcr1 <- sim_adpcr(m = 10, n = 765, times = 10000, pos_sums = FALSE, 
                       n_panels = number_of_exps)
-  adpcr2 <- sim_adpcr(m = 60, n = 765, times = 10000, pos_sums = FALSE, 
+  adpcr2 <- sim_adpcr(m = 40, n = 765, times = 10000, pos_sums = FALSE, 
                       n_panels = number_of_exps)
   dat <- bind_dpcr(adpcr1, adpcr2)
   modeltab <- slot(test_counts(dat), "group_coef")
